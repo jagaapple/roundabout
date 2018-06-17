0d3ce2b6d662885bf3216b6a31ba2fe3994b4637
Jaga Apple <jagaapple@uniboar.com>
2018-06-17 18:40:26 +0900

    [Add] Create an empty project
    - Create an empty project
    - Add a gitignore file

---
+++ Roundabout/Roundabout.h
@@ -0,0 +1,19 @@
+//
+//  Roundabout.h
+//  Roundabout
+//
+//  Created by Jaga Apple on 6/17/30 H.
+//  Copyright © 30 Heisei Jaga Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Roundabout.
+FOUNDATION_EXPORT double RoundaboutVersionNumber;
+
+//! Project version string for Roundabout.
+FOUNDATION_EXPORT const unsigned char RoundaboutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Roundabout/PublicHeader.h>
+
+
