79acea85c266bf257f0272d3104ad1ad33a215e9
Jaga Apple <jagaapple@uniboar.com>
2018-07-15 17:40:32 +0900

    [Clean] Define a method to create State Signal independently to Store
    - Define a method to create State Signal independently to Store
    - Add documentation comments to it
    - Add StateSignalManager class in order to manage State Signals registered to Store

---
+++ Roundabout/Signals/StateSigalManager.swift
@@ -0,0 +1,43 @@
+// =============================================================================================================================
+// ROUNDABOUT - SIGNALS - STATE SIGNAL MANAGER
+// =============================================================================================================================
+import Foundation
+
+internal final class StateSignalManager {
+
+  // ---------------------------------------------------------------------------------------------------------------------------
+  // MARK: - Variables
+  // ---------------------------------------------------------------------------------------------------------------------------
+  // MARK: Internal Variables
+  internal static let shared: StateSignalManager = StateSignalManager()
+
+  // MARK: Private Variables
+  private var signals: [StoreSubscriberId: [StateSignalType]] = [:]
+
+
+  // ---------------------------------------------------------------------------------------------------------------------------
+  // MARK: - Functions
+  // ---------------------------------------------------------------------------------------------------------------------------
+  // MARK: Initializers
+  // ---------------------------------------------------------------------------------------------------------------------------
+  private init() {
+    // Disable an internal initializer in order to implement Singleton class.
+  }
+
+  // MARK: Internal Functions
+  // ---------------------------------------------------------------------------------------------------------------------------
+  internal func register(signals: [StateSignalType], of subscriberId: StoreSubscriberId) {
+    self.signals[subscriberId] = signals
+  }
+
+  internal func removeSignals(of subscriberId: StoreSubscriberId) {
+    self.signals.removeValue(forKey: subscriberId)
+  }
+
+  internal func inputAll(state: Any) {
+    self.signals.forEach({ (_, signals: [StateSignalType]) in
+      signals.forEach({ (signal: StateSignalType) in signal.input(state) })
+    })
+  }
+
+}
---
+++ Roundabout/Signals/Store+StateSignal.swift
@@ -0,0 +1,69 @@
+// =============================================================================================================================
+// ROUNDABOUT - SIGNALS - STORE + STATE SIGNAL
+// =============================================================================================================================
+import Foundation
+
+extension Store {
+
+  // ---------------------------------------------------------------------------------------------------------------------------
+  // MARK: - Variables
+  // ---------------------------------------------------------------------------------------------------------------------------
+  static fileprivate var handlerId: HandlerId { return "Roundabout/StateSignal" }
+
+
+  // ---------------------------------------------------------------------------------------------------------------------------
+  // MARK: - Functions
+  // ---------------------------------------------------------------------------------------------------------------------------
+  // MARK: Public Functions
+  // ---------------------------------------------------------------------------------------------------------------------------
+  /// Creates a StateSignal optimized for a Store. When suscribing a Store with created signals by this method, signal's raw
+  /// values are set from Application State automatically.
+  ///
+  /// ```swift
+  /// let userNameSignal = store.createSignal { $0.user.name }
+  /// store.subscribe(self, connectTo: [userNameSignal])
+  /// ```
+  ///
+  /// - Parameter source: A closure to return a value in order to set a new value from Application State.
+  /// - Returns: A StateSignal in order to set when subscribing a Store.
+  public func createSignal<T: Equatable>(
+    _ source: @escaping ((ApplicationStateType) -> T)
+  ) -> StateSignal<T, ApplicationStateType> {
+    let defaultValue: T = source(self.applicationState)
+    return StateSignal<T, ApplicationStateType>(defaultValue, source: source)
+  }
+
+  /// Subscribes in order to detect dispatchings and value changed using StateSignals. To avoid memory leaks, unsubscribe when
+  /// detection become unnecessary, but you don't need to unsubscribe set StateSignals directly.
+  ///
+  /// ```swift
+  /// store.subscribe(self, connectTo: [userNameSignal, userAgeSignal]) // Two signals are set.
+  /// userNameSignal.subscribe(self, didChange: { (_) in ... }) // You can subscribe the signals.
+  /// ...
+  /// store.unsubscribe(self) // The two signals are unsubscribed automatically.
+  /// userNameSignal.unsubscribe(self) // This is not needed.
+  /// ```
+  ///
+  /// - Parameters:
+  ///   - subscriber: Class or object in order to distinguish who has signals.
+  ///   - signals: Target StateSignals in order to set values from Application State automatically. These signals are
+  ///              unsubscribed when `store.unsubscribe(_:)` is called.
+  public func subscribe(_ subscriber: AnyObject, connectTo signals: [StateSignalType]) {
+    self.subscribe(subscriber, didChange: { (_) in })
+
+    let subscriberId: StoreSubscriberId = self.getSubscriberId(of: subscriber)
+    StateSignalManager.shared.register(signals: signals, of: subscriberId)
+
+    // Set a handler.
+    self.setDidDispatchHandler(id: Store.handlerId, handler: { (newState: ApplicationStateType, _, _) in
+      StateSignalManager.shared.inputAll(state: newState)
+    })
+    let uniqueHandlerIdWithSubscriberId: HandlerId = "\(Store.handlerId)#\(String(subscriberId.hashValue))"
+    self.setDidUnsubscribeHandler(id: uniqueHandlerIdWithSubscriberId, handler: { (id: StoreSubscriberId) in
+      if subscriberId != id { return }
+
+      StateSignalManager.shared.removeSignals(of: subscriberId)
+    })
+  }
+
+}
